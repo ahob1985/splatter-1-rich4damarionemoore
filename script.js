aa41ebf69c7d68146577d5badddbe4228fa5ac3c
ahob85 <aaron@ahob85.com>
2021-06-15 18:45:13 +0000

    got drawEllipse() working

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let buttonDiv;
+let clearButton;
+let sliderDiv;
+let slider;
+let minSpan;
+let maxSpan;
+
 
 function setup() {
   // create canvas UI
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  background(65, 60, 88);
+  canvas.mousePressed(drawEllipse);
+  canvas.parent(canvasDiv);
   // create slider UI
 
   // create button UI
-
+  buttonDiv = createDiv();
+  clearButton = createButton("Clear Canvas");
+  clearButton.mousePressed(function() {
+    background(65, 60, 88);
+  });
 }
 
 function draw() {
@@ -17,7 +33,8 @@ function draw() {
 }
 
 function drawEllipse() {
-
+  fill("#ff0");
+  ellipse(mouseX, mouseY, 100);
 }
 
 
@@ -38,5 +55,6 @@ function drawEllipse() {
 *******************************************************************************/
 
 function drawSplatter(){
-
+  fill(random(100, 256), random(100, 256), random(100, 256));
+  noStroke();
 }
